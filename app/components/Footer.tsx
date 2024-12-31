f7c08e275b26341a1593fe8f4c670720ab47571d
kylersaunders <kylersaunders@gmail.com>
2024-12-30 22:27:41 -0800

    ...

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { useAppSelector } from '@/lib/hooks';
-import Autoplay from '../Autoplay';
+import Autoplay from './Autoplay';
 import { selectSelectedUrl } from '@/lib/features/recommendations/recommendationsSlice';
-import SeedTracks from '../recommendations/components/seed-tracks';
+import SeedTracks from './recommendations/components/seed-tracks';
 
 export default function Footer() {
   // const url = useAppSelector(selectSelectedUrl);
